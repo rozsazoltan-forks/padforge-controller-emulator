111dc4089606f42e16ace01dc9d2f9be191c6021
hifihedgehog <16614343+hifihedgehog@users.noreply.github.com>
2026-06-19 00:29:22 -0400

    Bump version to 3.5.0

--- SharedVersion.cs
+++ SharedVersion.cs
@@ -11,5 +11,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("3.4.0.0")]
-[assembly: AssemblyFileVersion("3.4.0.0")]
+[assembly: AssemblyVersion("3.5.0.0")]
+[assembly: AssemblyFileVersion("3.5.0.0")]
